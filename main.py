625319fd981e91903d25a4e4bda42aa5d41ea35f
Sergey Iguminov <Sergey.iguminov@yandex.ru>
2026-01-13 14:48:32 +0300

    Исправлена ошибка запуска приложения

---
+++ main.py
@@ -0,0 +1,11 @@
+from valutatrade_hub.cli.interface import run_cli
+
+
+def main():
+    """Точка входа в систему."""
+    print("ValutaTrade запущен.")
+    run_cli()
+
+
+if __name__ == "__main__":
+    main()
